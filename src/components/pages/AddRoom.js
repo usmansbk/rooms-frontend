eb791b23442920f08f7ea18968724f6df0fd8f4f
olawale-o <omoogunolawale@gmail.com>
2021-12-03 15:49:24 +0100

    Display Add room form

--- src/components/pages/AddRoom.js
+++ src/components/pages/AddRoom.js
@@ -1,3 +1,19 @@
-const AddRoom = () => <div>Add Room</div>;
+const AddRoom = () => {
+  return(
+    <div>
+      <h3>Add Room</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Name" {...register('name', { required: true })} />
+        <input placeholder="City" {...register('city', { required: true })} />
+        <input type="number" placeholder="Price" {...register('price', { required: true })} />
+        <input type="number" placeholder="Size" {...register('size', { required: true })} />
+        <input placeholder="Bed Type" {...register('bed_type', { required: true })} />
+        <textarea placeholder="Facilities" {...register('facilities', { required: true })} />
+        <input placeholder="Picture" type="file" accept="image/png, image/jpeg" {...register('picture', { required: true })} />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
 
 export default AddRoom;
