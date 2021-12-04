bbf924122563cdf68e85c86f99d11cdadf03023f
Usman Suleiman <usmansbk@gmail.com>
2021-12-04 15:55:34 +0100

    Fetch initial data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, Outlet } from 'react-router-dom';
+import { fetchRooms } from '../redux/rooms';
 import { signout } from '../redux/auth';
 
 const links = [
@@ -30,14 +31,20 @@ const Home = () => {
   const dispatch = useDispatch();
   const handleSignout = useCallback(() => dispatch(signout), []);
 
+  useEffect(() => {
+    dispatch(fetchRooms);
+  }, []);
+
   return (
     <div>
-      <nav>
-        <h5>Room App</h5>
-        {links.map(({ to, name }) => <Link key={to} to={to}>{name}</Link>)}
-        <button type="button" onClick={handleSignout}>Sign out</button>
-      </nav>
-      <Outlet />
+      <div>
+        <nav>
+          <h5>Room App</h5>
+          {links.map(({ to, name }) => <Link key={to} to={to}>{name}</Link>)}
+          <button type="button" onClick={handleSignout}>Sign out</button>
+        </nav>
+        <Outlet />
+      </div>
     </div>
   );
 };
