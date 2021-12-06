88d051848fffa3a237571c1aad8de553d9984b82
denisdiaconu <denisdiaconu117@yahoo.com>
2021-12-06 09:36:45 +0100

    Add DeleteRoom component

--- src/components/pages/DeleteRoom.js
+++ src/components/pages/DeleteRoom.js
@@ -1,3 +1,27 @@
-const DeleteRoom = () => <div>Delete room</div>;
+/* eslint-disable react/prop-types */
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteRoom } from '../../redux/rooms';
+import { selectAllRooms, selectRoomById } from '../../redux/rooms/selectors';
+
+const Item = ({ onDelete, id }) => {
+  const { name } = useSelector(selectRoomById(id));
+
+
+const DeleteRoom = () => {
+  const roomIds = useSelector(selectAllRooms);
+  const dispatch = useDispatch();
+
+  const handleDelete = useCallback((id) => dispatch(deleteRoom(id)), []);
+
+  return (
+    <div>
+      <h3>Delete Room</h3>
+      <div>
+        {roomIds.map((id) => <Item key={id} id={id} onDelete={handleDelete} />)}
+      </div>
+    </div>
+  );
+};
 
 export default DeleteRoom;
