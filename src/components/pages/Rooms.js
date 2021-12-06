9d076a55e5333c64135cd5a6a971c90114bbc727
denisdiaconu <denisdiaconu117@yahoo.com>
2021-12-06 09:43:09 +0100

    Add Rooms component

--- src/components/pages/Rooms.js
+++ src/components/pages/Rooms.js
@@ -1,3 +1,32 @@
-const Rooms = () => <div>Rooms</div>;
+/* eslint-disable react/prop-types */
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { BASE_URL } from '../../constants';
+import { selectAllRooms, selectRoomById } from '../../redux/rooms/selectors';
+
+const Card = ({ id }) => {
+  const { name, picture, facilities } = useSelector(selectRoomById(id));
+
+  return (
+    <Link to={`/rooms/${id}`}>
+      <div>
+        <img alt="" src={`${BASE_URL}${picture}`} style={{ width: 300, height: 300 }} />
+        <h5>{name}</h5>
+        <p>{facilities}</p>
+      </div>
+    </Link>
+  );
+};
+
+const Rooms = () => {
+  const roomIds = useSelector(selectAllRooms);
+
+  return (
+    <div>
+      <h3>Available Rooms</h3>
+      {roomIds.map((id) => <Card id={id} key={id} />)}
+    </div>
+  );
+};
 
 export default Rooms;
