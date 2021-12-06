6e37a6952fdf31cd882a7009859ce3ef30edf3f0
olawale-o <omoogunolawale@gmail.com>
2021-12-06 05:21:00 +0100

    Create a selector to select rooms by city from store

--- src/redux/rooms/selectors.js
+++ src/redux/rooms/selectors.js
@@ -1,2 +1,8 @@
 export const selectAllRooms = (state) => state.rooms.allIds;
 export const selectRoomById = (id) => (state) => state.rooms.byId[id];
+export const selectRoomsByCity = (currentCity) => (state) => (
+  state.rooms.allIds.filter((id) => {
+  const { city } = state.rooms.byId[id];
+  
+  return city === currentCity;
+}));
