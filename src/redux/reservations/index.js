71d3eb30327614d360968a410d4f531a1f5e330f
olawale-o <omoogunolawale@gmail.com>
2021-12-06 05:50:09 +0100

    Create LOAD_RESERVATIONS action type

--- src/redux/reservations/index.js
+++ src/redux/reservations/index.js
@@ -1,3 +1,11 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { RESET } from '../actions';
+import * as API from '../../API';
+import { normailzeReservation, normailzeReservations } from '../schema';
+import { addRoom } from '../rooms';
+
+const LOAD_RESERVATIONS = 'reservations/load';
+
 const reducer = (state = {}, action) => {
   switch (action.type) {
     default:
