b56af5c459b9991fd00ced050a0f793e505a5331
denisdiaconu <denisdiaconu117@yahoo.com>
2021-12-06 09:40:09 +0100

    Add Reservations component

--- src/components/pages/Reservations.js
+++ src/components/pages/Reservations.js
@@ -5,6 +5,39 @@ import { deleteReservation, fetchReservations } from '../../redux/reservations';
 import { selectMyReservations, selectReservationById } from '../../redux/reservations/selectors';
 import { selectRoomById } from '../../redux/rooms/selectors';
 
-const Reservations = () => <div>Reservations</div>;
+const Item = ({ id, onDelete }) => {
+  const { check_in: checkIn, nights, room } = useSelector(selectReservationById(id));
+  const { city } = useSelector(selectRoomById(room));
+  return (
+    <div>
+      <div>{checkIn}</div>
+      <span>{nights}</span>
+      <p>{city}</p>
+      <button type="button" onClick={() => onDelete(id)}>
+        Cancel
+      </button>
+    </div>
+  );
+};
+
+const Reservations = () => {
+  const dispatch = useDispatch();
+  const reservationIds = useSelector(selectMyReservations);
+
+  useEffect(() => {
+    dispatch(fetchReservations);
+  }, []);
+
+  const handleDelete = useCallback((id) => dispatch(deleteReservation(id)), []);
+
+  return (
+    <div>
+      <h3>My Reservations</h3>
+      <ul>
+        {reservationIds.map((id) => <Item key={id} id={id} onDelete={handleDelete} />)}
+      </ul>
+    </div>
+  );
+};
 
 export default Reservations;
