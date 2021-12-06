0075f519bd03b87245085630ffa76587933dd522
denisdiaconu <denisdiaconu117@yahoo.com>
2021-12-06 09:41:53 +0100

    Add Room imports

--- src/components/pages/Room.js
+++ src/components/pages/Room.js
@@ -1,3 +1,11 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { BASE_URL } from '../../constants';
+import { fetchRoom } from '../../redux/rooms';
+import { selectRoomById } from '../../redux/rooms/selectors';
+import ReserveForm from '../common/ReserveForm';
+
 const Room = () => <div>Room</div>;
 
 export default Room;
