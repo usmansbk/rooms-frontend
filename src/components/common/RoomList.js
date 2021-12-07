58fb67bb8e2760eacf039bbba70a69613dc70d9b
Usman Suleiman <usmansbk@gmail.com>
2021-12-07 22:22:47 +0100

    Refactor

--- src/components/common/RoomList.js
+++ src/components/common/RoomList.js
@@ -1,9 +1,25 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { BASE_URL } from '../../constants';
 import { selectRoomById } from '../../redux/rooms/selectors';
 import classes from '../../styles/RoomList.module.css';
+
+const CardFooter = () => (
+  <div className="flex-direction-row my-4 justify-content-center gap-4">
+    <div className={classes.icon}>
+      <FontAwesomeIcon icon={faFacebookF} />
+    </div>
+    <div className={classes.icon}>
+      <FontAwesomeIcon icon={faTwitter} />
+    </div>
+    <div className={classes.icon}>
+      <FontAwesomeIcon icon={faInstagram} />
+    </div>
+  </div>
+);
 
 const Card = ({ id }) => {
   const { name, picture, facilities } = useSelector(selectRoomById(id));
@@ -13,8 +29,11 @@ const Card = ({ id }) => {
       <div className={classes.card}>
         <img alt="" src={`${BASE_URL}${picture}`} className={classes.image} />
         <h5 className="h4 mt-4 fw-600">{name}</h5>
-        <div className="dot-divider my-4" />
-        <p className="caption fw-600 text-gray text-align-center">{facilities}</p>
+        <div className={classes.divider} />
+        <div className="text-gray">
+          <p className="caption fw-600 text-align-center">{facilities}</p>
+          <CardFooter />
+        </div>
       </div>
     </Link>
   );
--- src/components/pages/Rooms.js
+++ src/components/pages/Rooms.js
@@ -6,7 +6,7 @@ const Rooms = () => {
   const roomIds = useSelector(selectAllRooms);
 
   return (
-    <div className="container p-4 justify-content-center">
+    <div className="container pt-4 justify-content-center">
       <h3 className="h3 mb-10 text-align-center">AVAILABLE ROOMS</h3>
       <RoomList data={roomIds} />
     </div>
