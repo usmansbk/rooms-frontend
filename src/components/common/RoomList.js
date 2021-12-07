0b01416c4f22f5943f6796cb7989d7c4b6e7dfa8
Simon Grchevski <simongrcevski@gmail.com>
2021-12-08 00:03:46 +0100

    Add Card

--- src/components/common/RoomList.js
+++ src/components/common/RoomList.js
@@ -1,7 +1,11 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import classes from '../../styles/RoomList.module.css';
+import { BASE_URL } from '../../constants';
+import { selectRoomById } from '../../redux/rooms/selectors';
+
 
 const CardFooter = () => (
   <div className="flex-direction-row my-4 justify-content-center gap-4">
@@ -16,3 +20,21 @@ const CardFooter = () => (
     </div>
   </div>
 );
+
+const Card = ({ id }) => {
+  const { name, picture, facilities } = useSelector(selectRoomById(id));
+
+  return (
+    <Link to={`/rooms/${id}`}>
+      <div className={classes.card}>
+        <img alt="" src={`${BASE_URL}${picture}`} className={classes.image} />
+        <h5 className="h4 mt-4 fw-600">{name}</h5>
+        <div className={classes.divider} />
+        <div className="text-gray">
+          <p className="caption fw-600 text-align-center">{facilities}</p>
+          <CardFooter />
+        </div>
+      </div>
+    </Link>
+  );
+};
