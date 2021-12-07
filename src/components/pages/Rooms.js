47a313eeaba764640d41330ef60dccf10767fea4
Simon Grchevski <simongrcevski@gmail.com>
2021-12-07 22:57:08 +0100

    Add the stylings for the rooms

--- src/components/pages/Rooms.js
+++ src/components/pages/Rooms.js
@@ -3,16 +3,17 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { BASE_URL } from '../../constants';
 import { selectAllRooms, selectRoomById } from '../../redux/rooms/selectors';
+import '../../styles/rooms.css';
 
 const Card = ({ id }) => {
   const { name, picture, facilities } = useSelector(selectRoomById(id));
 
   return (
     <Link to={`/rooms/${id}`}>
-      <div>
-        <img alt="" src={`${BASE_URL}${picture}`} style={{ width: 300, height: 300 }} />
-        <h5>{name}</h5>
-        <p>{facilities}</p>
+      <div className="room-container gap-2 p-4">
+        <img alt="" src={`${BASE_URL}${picture}`} style={{ width: 300, height: 300 }} className="room-image" />
+        <h5 className="h3">{name}</h5>
+        <p className="room-facility footer-text">{facilities}</p>
       </div>
     </Link>
   );
@@ -22,9 +23,11 @@ const Rooms = () => {
   const roomIds = useSelector(selectAllRooms);
 
   return (
-    <div>
-      <h3>Available Rooms</h3>
-      {roomIds.map((id) => <Card id={id} key={id} />)}
+    <div className="rooms-wrapper">
+      <h1 className="">Available Rooms</h1>
+      <div className="rooms-container gap-2 p-2">
+        {roomIds.map((id) => <Card id={id} key={id} />)}
+      </div>
     </div>
   );
 };
