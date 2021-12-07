ed3de5a1f04d6f8b1e929faf98b82d489de39d1f
Simon Grchevski <simongrcevski@gmail.com>
2021-12-08 00:44:36 +0100

    Add styles for displaying rooms in a row

--- src/components/pages/Rooms.js
+++ src/components/pages/Rooms.js
@@ -1,33 +1,14 @@
-/* eslint-disable react/prop-types */
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { BASE_URL } from '../../constants';
-import { selectAllRooms, selectRoomById } from '../../redux/rooms/selectors';
-import '../../styles/rooms.css';
-
-const Card = ({ id }) => {
-  const { name, picture, facilities } = useSelector(selectRoomById(id));
-
-  return (
-    <Link to={`/rooms/${id}`}>
-      <div className="room-container gap-2 p-4">
-        <img alt="" src={`${BASE_URL}${picture}`} style={{ width: 300, height: 300 }} className="room-image" />
-        <h5 className="h3">{name}</h5>
-        <p className="room-facility footer-text">{facilities}</p>
-      </div>
-    </Link>
-  );
-};
+import { selectAllRooms } from '../../redux/rooms/selectors';
+import RoomList from '../common/RoomList';
 
 const Rooms = () => {
   const roomIds = useSelector(selectAllRooms);
 
   return (
-    <div className="rooms-wrapper">
-      <h1 className="">Available Rooms</h1>
-      <div className="rooms-container gap-2 p-2">
-        {roomIds.map((id) => <Card id={id} key={id} />)}
-      </div>
+    <div className="container pt-4 justify-content-center">
+      <h3 className="h3 mb-10 text-align-center">AVAILABLE ROOMS</h3>
+      <RoomList data={roomIds} />
     </div>
   );
 };
